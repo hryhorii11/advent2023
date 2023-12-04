0e4f99bebab5ed7e3b0fa11841b3f03b6a1bad55
hryhorii11 <124711455+hryhorii11@users.noreply.github.com>
2023-12-04 14:48:27 +0300

    Add files via upload

---
+++ day4/advent/src/main/kotlin/Main.kt
@@ -0,0 +1,62 @@
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
+fun main() {
+    val file =
+        File("inputData.txt")
+    val reader = BufferedReader(FileReader(file, Charsets.UTF_8))
+    val list = ArrayList<String>();
+    reader.lines().forEach {
+        list.add(it)
+    }
+    task1(list)
+    task2(list)
+}
+
+fun task1(list: ArrayList<String>) {
+
+    var sum = 0
+    for (line in list) {
+        val winNumbers = Regex("\\d+").findAll(line.substring(line.indexOf(':'), line.indexOf('|')))
+        val myNumbers = Regex("\\d+").findAll(line.substring(line.indexOf('|')))
+        var matchNumbersCount = -1
+        for (n1 in winNumbers) {
+            for (n2 in myNumbers) {
+                if (n1.value.equals(n2.value)) {
+                    matchNumbersCount++
+                }
+            }
+        }
+        if (matchNumbersCount >= 0)
+            sum += Math.pow(2.0, matchNumbersCount.toDouble()).toInt()
+
+    }
+    println(sum)
+}
+
+
+fun task2(list: ArrayList<String>) {
+    var sum = 0
+    val cardList = IntArray(list.size) { 1 }
+    for (line in list) {
+        val winNumbers = Regex("\\d+").findAll(line.substring(line.indexOf(':'), line.indexOf('|')))
+        val myNumbers = Regex("\\d+").findAll(line.substring(line.indexOf('|')))
+        var b = 0
+        for (n1 in winNumbers) {
+            for (n2 in myNumbers) {
+                if (n1.value.equals(n2.value)) {
+                    for (i in 0..cardList[list.indexOf(line)] - 1)
+                        cardList[list.indexOf(line) + b + 1]++
+                    b++
+                }
+            }
+        }
+    }
+    for (n in cardList) {
+        sum += n
+    }
+    println(sum)
+}
+
+
